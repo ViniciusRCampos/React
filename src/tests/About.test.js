25a7afe78cefb8680890e71fdf5a63e3b4fd24b8
ViniciusRCampos <vinicius_uo@hotmail.com>
2022-09-19 15:43:17 -0300

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,33 @@
-test('Teste o componente <App.js />', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+// import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import About from '../pages/About';
+
+describe('Testando o componente About', () => {
+  test('Teste se a página contém as informações sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+
+    const pokedex = screen.getByRole('heading', { name: /about pokédex/i, level: 2 });
+    expect(pokedex).toBeInTheDocument();
+  });
+
+  test('Teste se a página contém a seguinte imagem de uma Pokédex:', () => {
+    renderWithRouter(<About />);
+
+    const imageLink = screen.getByRole('img');
+    expect(imageLink).toBeInTheDocument();
+    expect(imageLink).toHaveAttribute(
+      'src',
+      'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png',
+    );
+  });
+
+  test('Se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+
+    const infoPokedex = screen.getAllByText(/pokémons/i);
+    expect(infoPokedex).toHaveLength(2);
+  });
+});
