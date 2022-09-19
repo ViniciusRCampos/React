61d08892a6e62a57296cd1034b2951db0d634a3d
ViniciusRCampos <vinicius_uo@hotmail.com>
2022-09-19 16:42:43 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+// import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../pages/NotFound';
+
+describe('Teste o componente <NotFound.js />', () => {
+  test(
+    'Teste se a página contém um heading h2 com o texto Page requested not found',
+    () => {
+      renderWithRouter(<NotFound />);
+
+      const textNotFound = screen.getByRole(
+        'heading',
+        { name: /page requested not found/i, level: 2 },
+      );
+      expect(textNotFound).toBeInTheDocument();
+    },
+  );
+  test('Teste se a pagina mostra uma imagem', () => {
+    renderWithRouter(<NotFound />);
+
+    const url = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const image = screen.getByRole('img');
+
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute('src', url);
+  });
+});
